668b2b63f6a6f9673ed92790448aa0355b4f2837
eungi <eungi429@gmail.com>
2021-07-16 01:30:19 +0900

    mes1

---
+++ mes.js
@@ -0,0 +1,7 @@
+let mes = {
+    subject: ['I','You','He','She','They','It','The God'],
+    mverb: ['can','could','should','will','shall','must','may',''],
+    verb: ['do','fuck','love','go through','help'],
+    object: ['pizza','her','book','you']
+}
+
